09e506ab39eba61a1661d546bcd5811af16764be
Rodrigo Amaral <Rodrigo.Amaral@micromed.com>
2018-12-04 16:20:45 -0200

    Commit

--- ReadSignals.py
+++ ReadSignals.py
@@ -4,18 +4,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def Read(PathSignal):
-    signals, fields = wfdb.rdsamp(PathSignal)
-    #print(np.shape(signals))
+    signals, fields = wfdb.rdsamp(PathSignal, channel_names='PLETH' )
+    print(fields)
+    #signals, fields = wfdb.rdsamp(PathSignal, channels=[1])
+    #print(fields)
+    #for x in fields['sig_name']:
+    #    if x == 'II':
+    #        ECG = 'II'
+    #    if x == 'PLETH':
+    #        PPG = 'PLETH'
+    #    if x == 'ABP':
+    #        ABP = 'ABP'
+    #print(np.shape(signals))	# Tamanho do sinal
     #plt.plot(signals[:,2])
     #plt.show()
 	
 	#https://www.w3schools.com/python/python_dictionaries.asp
-    for x in fields.items():
-        print(x)
-    ECG = 1
-    PPG = 2
-    ABP = 3
-    return ECG, PPG, ABP
+    #for x in fields.items():
+    #    print(x)
+	
+    ECG=1 
+    PPG=2 
+    ABP=3
+    FS=4
+
+    return ECG, PPG, ABP, FS
 	
 
 
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import fibo
 import ReadSignals
 
 def main():
-    ECG, PPG, ABP = ReadSignals.Read('3000063_0010')
+    ECG, PPG, ABP, FS = ReadSignals.Read('3000063_0010')
     print(ECG)
     print(PPG)
     print(ABP)
+    print(FS)
 	#signals, fields = wfdb.rdsamp('3000063_0010')
 	#wfdb.plot_all_records(directory='matlab')
 	
